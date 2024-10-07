9ca044554524f24b48e268051d78fcdfcecd1d92
raheem <raheemideadev@gmail.com>
2024-10-07 22:40:50 +0500

    Installed dependencies;

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,24 +1,41 @@
+@file:OptIn(ExperimentalKotlinGradlePluginApi::class, ExperimentalComposeLibrary::class)
+
+import org.jetbrains.compose.ExperimentalComposeLibrary
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSetTree
 
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.android)
+    alias(libs.plugins.compose)
     alias(libs.plugins.compose.compiler)
 }
 
 kotlin {
     androidTarget {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_11)
         }
+
+        instrumentedTestVariant {
+            sourceSetTree.set(KotlinSourceSetTree.test)
+
+            dependencies {
+
+                // core
+                implementation(libs.core)
+
+                // compose test
+                implementation(libs.compose.test.junit4)
+                implementation(libs.compose.test.manifest)
+            }
+        }
     }
-    
+
     jvm("desktop")
-    
+
     listOf(
         iosX64(),
         iosArm64(),
@@ -29,34 +46,62 @@ kotlin {
             isStatic = true
         }
     }
-    
+
     sourceSets {
         val desktopMain by getting
-        
+
         androidMain.dependencies {
+
+            // compose preview
             implementation(compose.preview)
-            implementation(libs.androidx.activity.compose)
+
+            // compose activity
+            implementation(libs.compose.activity)
         }
+
         commonMain.dependencies {
+
+            // compose
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material)
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
-            implementation(libs.androidx.lifecycle.viewmodel)
-            implementation(libs.androidx.lifecycle.runtime.compose)
         }
+
+        commonTest.dependencies {
+
+            // kotlin test
+            implementation(libs.kotlin.test)
+            implementation(kotlin("test-annotations-common"))
+
+            // assertk
+            implementation(libs.assertk)
+
+            // compose test
+            implementation(compose.uiTest)
+        }
+
         desktopMain.dependencies {
+
+            // compose current os
             implementation(compose.desktop.currentOs)
-            implementation(libs.kotlinx.coroutines.swing)
         }
+    }
+
+    compilerOptions {
+        freeCompilerArgs.addAll(
+            listOf(
+                "-Xexpect-actual-classes"
+            )
+        )
     }
 }
 
 android {
     namespace = "xyz.teamgravity.kotlinmultiplatformtestdemo"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    compileSdk = libs.versions.sdk.compile.get().toInt()
 
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
     sourceSets["main"].res.srcDirs("src/androidMain/res")
@@ -64,29 +109,33 @@ android {
 
     defaultConfig {
         applicationId = "xyz.teamgravity.kotlinmultiplatformtestdemo"
-        minSdk = libs.versions.android.minSdk.get().toInt()
-        targetSdk = libs.versions.android.targetSdk.get().toInt()
+        minSdk = libs.versions.sdk.min.get().toInt()
+        targetSdk = libs.versions.sdk.target.get().toInt()
         versionCode = 1
         versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    buildFeatures {
-        compose = true
-    }
+
     dependencies {
+
+        // compose ui tooling
         debugImplementation(compose.uiTooling)
     }
 }
